3eb5883b38a17bafca4dc57b573ce94f4db7ce85
Summer Liddington <summerliddington@gmail.com>
2019-12-20 15:43:00 -0600

    added search by routes

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
+import SearchBar from './SearchBar';
 
 
 export default class NavigationBar extends Component {
@@ -9,15 +10,48 @@ export default class NavigationBar extends Component {
 
 
     render() {
+        
         return (
             <>
-                <h1>{this.props.view}</h1>
-                {(this.props.view === "Employees") ?
+                <h1>{this.props.portal}</h1>
+                {(this.props.portal === "Employees") ?
 
                     <button><Link to="/customer-portal">Go to Customer Portal</Link></button>
                     :
                     <button><Link to="/employee-portal">Go to Employee Portal</Link></button>
                 }
+                <Route
+					exact path="/customer-portal"
+					render={props => {
+						return <SearchBar {...props} view="customers" />;
+					}}
+				/>
+                <Route
+					path="/customer-portal/customers"
+					render={props => {
+						return <SearchBar {...props} view="customers" />;
+					}}
+				/>
+				<Route
+					path="/customer-portal/orders"
+					render={props => {
+						return <SearchBar {...props} view="orders" />;
+					}}
+				/>
+
+				<Route
+					path="/customer-portal/payments"
+					render={props => {
+						return null;
+					}}
+				/>
+
+				<Route
+					path="/customer-portal/products"
+					render={props => {
+						return <SearchBar {...props} view="products" />;
+					}}
+				/>
             </>
         )
     }
